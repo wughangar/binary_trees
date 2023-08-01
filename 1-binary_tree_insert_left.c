53fecdb86edfcd331a15c8c6e57f787dff3b540e
wughangar <wughangar.rose@gmail.com>
2023-08-01 12:12:00 +0300

    insert left 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,11 +11,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newnode = binary_tree_node(parent, value);
+
 	if (parent == NULL)
 	{
 		return (NULL);
 	}
-	binary_tree_t *newnode = binary_tree_node(parent, value);
 
 	if (newnode == NULL)
 	{
