722c55e632b5a79860f7d63e03483fcaf5242ca7
wughangar <wughangar.rose@gmail.com>
2023-08-02 09:45:44 +0300

    depth function

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,7 +2,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - funstion that measures the height of a binary tree
+ * binary_tree_depth - funstion that measures the height of a binary tree
  * @tree: pointer to root node of the tree
  *
  * Return: int
@@ -10,38 +10,11 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t ldepth = 0;
-	size_t rdepth = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	ldepth = binary_tree_depth(tree->left);
-	rdepth = binary_tree_depth(tree->right);
+	if (tree->parent == NULL)
+		return (0);
 
-	if (ldepth > rdepth)
-		return (ldepth + 1);
-	else
-		return (rdepth + 1);
-}
-
-int main(void)
-{
-    binary_tree_t *root;
-    size_t depth;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
-
-    depth = binary_tree_depth(root);
-    printf("Depth of %d: %lu\n", root->n, depth);
-    depth = binary_tree_depth(root->right);
-    printf("Depth of %d: %lu\n", root->right->n, depth);
-    depth = binary_tree_depth(root->left->right);
-    printf("Depth of %d: %lu\n", root->left->right->n, depth);
-    return (0);
+	return (1 + binary_tree_depth(tree->parent));
 }
